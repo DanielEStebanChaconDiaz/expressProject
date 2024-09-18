dd20ff62aa317aa3b7aeefecfad8b54dbdb1fcf7
Sh4un221 <127805282+Sh4un221@users.noreply.github.com>
2024-09-18 13:08:22 -0500

    feat: :construction: We are working on implementing user login through Instagram.

--- server/app.js
+++ server/app.js
@@ -1,14 +1,28 @@
 const express = require('express');
-const connectMongoDB = require('./server/config/config');
+const passport = require('passport');
+const session = require('express-session');
+const connectMongoDB = require('../server/config/config');
+
+const instagramRoutes = require('../server/routes/instagramRoutes'); 
 const app = express();
 require('dotenv').config();
-
 
 connectMongoDB();
 app.use(express.json());
 
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(instagramRoutes); 
+
 app.listen({
-    host: process.env.EXPRESS_HOST || 'localhost', 
+    host: process.env.EXPRESS_HOST || 'localhost',
     port: process.env.PORT || 3000
 }, () => {
     console.log(`Servidor corriendo en: http://localhost:${process.env.PORT || 3000}`);
---
+++ server/models/user.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const UsuarioSchema = new mongoose.Schema({
+  fotoPerfil: {type: String, sparse: true},
+  nombreUsuario: { type: String, unique: true, sparse: true },
+  correoElectronico: { type: String, sparse: true },
+  celular: { type: String, unique: true, sparse: true },
+  contrasena: { type: String },
+  sexo: { type: String },
+  fechaNacimiento: { type: Date },
+  metodosPago: [{
+    tipo: { type: String },
+    detalles: { type: String }
+  }],
+  productosComprados: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Producto' }],
+  talleresSuscritos: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Taller' }],
+
+  // Campos para autenticación de terceros
+  facebookId: { type: String, unique: true, sparse: true },
+  googleId: { type: String, unique: true, sparse: true },
+  instagramId: { type: String, unique: true, sparse: true },
+
+  // Campo para almacenar el avatar del usuario
+  avatarUrl: { type: String },
+
+  // Campo para almacenar datos adicionales de proveedores de autenticación
+  providerData: { type: mongoose.Schema.Types.Mixed }
+});
+
+const Usuario = mongoose.model('Usuario', UsuarioSchema); // Modelo en singular
+
+module.exports = Usuario;
