6b76172984c2b6ad36db1759d6e1051cf0ab9ee9
KarenYisethPintoEspejo <156373335+KarenYisethPintoEspejo@users.noreply.github.com>
2024-09-19 09:25:11 -0500

    feat: :sparkles: Construction finished home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,19 +2,20 @@ import '../styles/home.css'
 
 function App() {
   const categories = [
-    { name: 'Textilería', icon: 'bx bx-shirt' },
-    { name: 'Cerámica', icon: 'bx bx-bowl' },
-    { name: 'Orfebrería', icon: 'bx bx-gem' },
-    { name: 'Talla en piedra', icon: 'bx bx-scissors' },
-    { name: 'Talla en madera', icon: 'bx bx-tree' },
-    { name: 'Bordado', icon: 'bx bx-paint' },
-    { name: 'Joyería', icon: 'bx bx-ring' },
-    { name: 'Hojalatería', icon: 'bx bx-wrench' },
-    { name: 'Estampado', icon: 'bx bx-brush' },
-    { name: 'Pintura tradicional', icon: 'bx bx-palette' },
+    { name: 'Textilería', icon: '../../public/img/category1.svg' },
+    { name: 'Cerámica', icon: '../../public/img/category2.svg' },
+    { name: 'Orfebrería', icon: '../../public/img/category3.svg' },
+    { name: 'Talla en piedra', icon: '../../public/img/category4.svg' },
+    { name: 'Talla en madera', icon: '../../public/img/category5.svg' },
+    { name: 'Bordado', icon: '../../public/img/category6.svg'},
+    { name: 'Joyería', icon: '../../public/img/category7.svg' },
+    { name: 'Hojalatería', icon: '../../public/img/category8.svg' },
+    { name: 'Estampado', icon: '../../public/img/category9.svg' },
+    { name: 'Pintura tradicional', icon: '../../public/img/category10.svg' },
   ];
 
   return (
+    
     <div className="App">
       <header className="header">
         <div className="menu">
@@ -28,39 +29,42 @@ function App() {
           <img src="../../public/img/ubication.svg" alt="" /> Ubicación de entrega actual
         </button>
       </header>
-
       <section className="categories">
+        <h3>Categorias</h3>
+        <div className='category-box'>
         {categories.map((category, index) => (
           <div key={index} className="category-item">
-            <i className={category.icon}></i>
+            <img src={category.icon} alt={category.name} className="category-icon" />
             <span>{category.name}</span>
           </div>
         ))}
+        </div>
       </section>
 
       <section className="workshops">
-        <h2>Talleres del mes</h2>
+        <h3>Talleres del mes</h3>
         <p>¡Aprende cómo hacerlos en estos talleres educativos!</p>
-        <img src="https://via.placeholder.com/300" alt="Taller del mes" />
+        <img src="../../public/img/ejemplo1.svg" alt="Taller del mes" />
       </section>
 
       <footer className="footer-nav">
         <a href="#" className="nav-item">
-          <i className="bx bx-cog"></i>
+          <img src="../../public/img/icon-store.svg" alt="Store" />
         </a>
         <a href="#" className="nav-item">
-          <i className="bx bx-star"></i>
+          <img src="../../public/img/icon-offer.svg" alt="Offers" />
+        </a>
+        <a href="#" className="nav-item1">
+          <img src="../../public/img/icon-home.svg" alt="Home" />
         </a>
         <a href="#" className="nav-item">
-          <i className="bx bx-home"></i>
+          <img src="../../public/img/icon-car.svg" alt="Car" />
         </a>
         <a href="#" className="nav-item">
-          <i className="bx bx-bell"></i>
-        </a>
-        <a href="#" className="nav-item">
-          <i className="bx bx-user"></i>
+          <img src="../../public/img/icon-profile.svg" alt="Profile" />
         </a>
       </footer>
+
     </div>
   );
 }
