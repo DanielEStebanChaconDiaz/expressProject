44a1adb7d6ce72f0ebf5636f1a83198b5bd11027
CamiloEstebanConchaTorres <156373519+CamiloEstebanConchaTorres@users.noreply.github.com>
2024-09-25 12:07:20 -0500

    feat: :bug: fix a bug

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import axios from 'axios';
 import '../styles/login.css';
 
 export default function Login() {
--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,17 +1,30 @@
 import '../styles/register.css'
 
 export default function Register() {
+
+    const FacebookAuth = () => {
+        window.location.href = 'https://localhost:3000/auth/facebook';
+    };
+
+    const GoogleAuth = () => {
+        window.location.href = 'https://localhost:3000/auth/google';
+    };
+
+    const DiscordAuth = () => {
+        window.location.href = 'https://localhost:3000/auth/discord';
+    };
+
     return (
         <div className="container">
             <div className="container-top">
                 <h2>Regístrate ahora y obtén las mejores promociones en <strong>artesanías peruanas</strong></h2>
-                <button className="social-btn fb-btn">
+                <button className="social-btn fb-btn"onClick={FacebookAuth}>
                     <img src="../../public/img/facebook.svg" alt="" /> Regístrate con Facebook
                 </button>
-                <button className="social-btn insta-btn">
+                <button className="social-btn insta-btn"onClick={DiscordAuth}>
                     <img src="../../public/img/instagram.svg" alt="" /> Regístrate con Instagram
                 </button>
-                <button className="social-btn gmail-btn">
+                <button className="social-btn gmail-btn"onClick={GoogleAuth}>
                     <img src="../../public/img/gmail.svg" alt="" /> Regístrate con Gmail
                 </button>
                 <button className="social-btn mail-btn">
