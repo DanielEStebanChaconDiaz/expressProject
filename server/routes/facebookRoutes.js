ac9b016287638562cd0fb6f243ccb9debba489f9
CamiloEstebanConchaTorres <156373519+CamiloEstebanConchaTorres@users.noreply.github.com>
2024-09-26 09:30:22 -0500

    feat: :bug: Discord login bug fixed

--- server/routes/facebookRoutes.js
+++ server/routes/facebookRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email'] }));
 
 router.get('/auth/facebook/callback',
-    passport.authenticate('facebook', { failureRedirect: '/' }),
+    passport.authenticate('facebook', { failureRedirect: 'https://localhost:5000/#/home' }),
     (req, res) => {
         res.redirect('https://localhost:5000/#/home');
     }
