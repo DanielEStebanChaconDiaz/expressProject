ce1570d0c6dd6db388f042e2b2825df08dc8486b
Sh4un221 <127805282+Sh4un221@users.noreply.github.com>
2024-09-29 10:56:46 -0500

    feat: :construction: We are working on the logic for making purchases on the frontend.

--- src/pages/car.jsx
+++ src/pages/car.jsx
@@ -27,7 +27,7 @@ export default function Car() {
         withCredentials: true
       });
       
-      const { carrito } = response.data;
+      const { carrito } = response.data;  // Asume que la sesión contiene el carrito en el objeto carrito
       console.log('Carrito de la sesión:', carrito);
 
       const productosDetalles = await Promise.all(carrito.map(async (item) => {
@@ -36,12 +36,12 @@ export default function Car() {
         });
         return {
           ...item,
-          producto: productoResponse.data
+          producto: productoResponse.data // Agregar los detalles del producto
         };
       }));
 
-      setItems(productosDetalles || []);
-      calcularTotal(productosDetalles || []);
+      setItems(productosDetalles || []);  // Si no hay carrito, establece un array vacío
+      calcularTotal(productosDetalles || []);  // Calcula el total del carrito
       setError(null);
     } catch (error) {
       console.error('Error al obtener el carrito de la sesión:', error);
